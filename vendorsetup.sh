f803bdec641c57938348158d96d99a1093c3d2c1
iHSAN <ihsanulrahman@gmail.com>
2026-08-10 10:41:09 +0000

    untrack LFS
    Signed-off-by: iHSAN <ihsanulrahman@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,2 @@
+echo "Merging Miuicamera Parts"
+cat vendor/xiaomi/miuicamera-miatoll/proprietary/system/priv-app/MiuiCamera/MiuiCamera.apk.part** > vendor/xiaomi/miuicamera-miatoll/proprietary/system/priv-app/MiuiCamera/MiuiCamera.apk
